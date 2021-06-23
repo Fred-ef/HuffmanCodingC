85cc443657e7afc22a3241bd743d6ac3818e5320
FredFed <thenedbear@hotmail.it>
2021-06-24 00:01:35 +0200

    added data-frequency elaboration, added compression function

--- huffman.c
+++ huffman.c
@@ -1,7 +1,7 @@
 #include "huffman.h"
 
 // creates a heap-node from given arguments, allocating its memory
-hnode* create_node(unsigned char data, unsigned int frequency) {
+hnode* create_node(byte data, int frequency) {
   hnode* node = (hnode*)malloc(sizeof(hnode));    // generating the node
 
   node->data = data;    // store the data in its field as an ASCII character
@@ -12,21 +12,14 @@ hnode* create_node(unsigned char data, unsigned int frequency) {
 }
 
 // creates a heap-tree of a given capacity, allocating its memory
-heap* create_heap(unsigned int cap) {
+heap* create_heap(int cap) {
   heap* h = (heap*)malloc(sizeof(heap));   // generating the tree
 
-  h->dim=0;   // the heap initially has no nodes
+  h->size=0;   // the heap initially has no nodes
   h->cap=cap;    // setting the tree's capacity
   h->arr=(hnode**)malloc(h->cap*sizeof(hnode*));    // allocates an array of sub-trees
 
   return h;    // returning the tree built
-}
-
-// swaps the nodes inverting their pointers
-void swap_nodes(hnode** a, hnode** b) {
-  hnode* temp = *a;
-  *a=*b;
-  *b=temp;
 }
 
 void min_heapify(heap* h, int index) {
@@ -34,31 +27,29 @@ void min_heapify(heap* h, int index) {
   left=2*index+1;
   right=2*index+2;
 
-  if((left < h->dim) && (h->arr[left]->frequency < h->arr[index]->frequency)) min=left;
+  if((left < h->size) && (h->arr[left]->frequency < h->arr[index]->frequency)) min=left;
   else min=index;
 
-  if((right < h->dim) && (h->arr[right]->frequency < h->arr[min]->frequency)) min=right;
+  if((right < h->size) && (h->arr[right]->frequency < h->arr[min]->frequency)) min=right;
 
   if(min != index) {
-    swap_nodes(&(h->arr[min]), &(h->arr[index]));
+    hnode* temp = h->arr[min];
+    h->arr[min]=h->arr[index];
+    h->arr[index]=temp;
     min_heapify(h, min);    // recursive call
   }
 }
 
-int has_one(heap* h) {
-  return (h->dim == 1);    // returns true if the heap has size 1, false otherwise
-}
-
 hnode* get_min(heap* h) {
   hnode* temp = h->arr[0];
-  h->arr[0] = h->arr[(h->dim--) - 1];
+  h->arr[0] = h->arr[(h->size--) - 1];
   min_heapify(h, 0);
 
   return temp;
 }
 
 void insert_node(heap* h, hnode* node) {
-  int i = h->dim++;
+  int i = h->size++;
 
   while(i && node->frequency < h->arr[(i-1)/2]->frequency) {
     h->arr[i] = h->arr[(i-1)/2];
@@ -70,7 +61,7 @@ void insert_node(heap* h, hnode* node) {
 
 void build_min_heap(heap* h) {
   int i;    // for-loop index
-  int d = h->dim-1;
+  int d = h->size-1;
 
   for(i=(d-1)/2; i>=0; i--) min_heapify(h, i);
 }
@@ -83,27 +74,41 @@ void print_arr(int v[], int n) {
   printf("\n");
 }
 
+void print_arr_c(byte* v, int size) {
+  int i;
+  for(i=0; i<size; i++) {
+    printf("Posizione %d: %d\n", i, v[i]);
+  }
+
+  return;
+}
+
 int is_leaf(hnode* n) {
   return ((n->left==NULL) && (n->right==NULL));
 }
 
-heap* create_build_min_heap(unsigned char* data, int* frequency, int dim) {
-  int i;    // for-loop index
-  heap* h = create_heap(dim);   // creates a min-heap of capacity=cap
+heap* create_build_min_heap(byte* data, int actual_size) {
+  int i, j;    // for-loop indexes
+  heap* h = create_heap(actual_size);   // creates a min-heap of capacity=cap
 
-  for(i=0; i<dim; i++) h->arr[i] = create_node(data[i], frequency[i]);
+  for(i=0, j=0; i<MAX_UCHAR && j<actual_size; i++) {
+    if(data[i]!=0) {    // if the element's frequency is not null...
+      h->arr[j] = create_node(i, data[i]);    // ...creates a node out of it and inserts it in the heap
+      j++;
+    }
+  }
 
-  h->dim = dim;
-  build_min_heap(h);
+  h->size = actual_size;   // updating the tree's size
+  build_min_heap(h);    // building a min-heap out of the tree
 
-  return h;
+  return h;   // returns the min-heap constructed
 }
 
-hnode* build_huff_tree(unsigned char* data, int* frequency, int dim) {
+hnode* build_huff_tree(byte* data, int actual_size) {
   hnode *left, *right, *merge;
-  heap* h = create_build_min_heap(data, frequency, dim);    // builds a min-heap of given capacitys
+  heap* h = create_build_min_heap(data, actual_size);    // builds a min-heap of given capacity
 
-  while(!has_one(h)) {    // iterates untill the heap has a single element
+  while(!(h->size == 1)) {    // iterates untill the heap has a single element
     left=get_min(h);    // extracts the less frequent element from the tree
     right=get_min(h);   // extracts the second less frequent element from the tree
 
@@ -133,19 +138,45 @@ void print_codes(hnode* r, int* v, int m) {
   }
 }
 
-void alpha_encode(unsigned char* data, int* frequency, int dim) {
-  hnode* root=build_huff_tree(data, frequency, dim);
-  int v[255], m=0;
+static byte get_size(byte* buf) {
+  int i;    // for-loop index
+  byte size=0;    // holds the number of elements with non-zero frequency
+
+  for(i=0; i<MAX_UCHAR; i++) {
+    if(buf[i]!=0) size++;   // for every element present, increase size by 1
+  }
+
+  return size;    // returns the size of the non-null part of the array
+}
+
+byte* process_data(byte* buf, int size) {
+  int i;    // for-loop index
+  byte* freq = (byte*)malloc(MAX_UCHAR*sizeof(byte));    // allocates a byte array with a cell for each possible byte value
+  memset(freq, 0, MAX_UCHAR);   // initializing every element of the array to 0
+
+  for(i=0; i<size; i++) {
+    freq[buf[i]]++;
+  }
+
+  return freq;    // returns the constructed array, ready to be encoded
+}
+
+void compress(byte* buf, int size) {
+
+  byte* data=process_data(buf, size);    // constructs an array out of buffer values' frequencies
+  byte num_different_elements=get_size(data);   // gets the number of different elements in the array
+  hnode* root=build_huff_tree(data, num_different_elements);    // builds a huffman tree out of the data extracted
+
+  int v[256], m=0;
   print_codes(root, v, m);
+
+  return;
 }
 
 int main() {
-  unsigned char data[] = {1, 2, 3, 4, 5, 6};
-  int frequency[] = {5, 9, 12, 13, 16, 45};
-
-  int size = sizeof(data)/sizeof(data[0]);
-
-  alpha_encode(data, frequency, size);
+  byte fl[] = {1, 4, 5, 5, 2, 9, 21, 15, 18, 33, 74, 1, 2, 2, 15, 18, 25, 27, 30, 40};
+  
+  compress(fl, sizeof(fl));
 
   return 0;
 }
--- huffman.h
+++ huffman.h
@@ -5,35 +5,44 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_UCHAR 256   // defines the maximum value for the byte (unsigned char) data type
+
+typedef unsigned char byte;   // type of data use to represent raw bytes
 
 typedef struct heap_node {   // implementation of a heap sub-tree
-  unsigned char data;    // 8 bits of data interpreted as a raw bytes
+  byte data;    // 8 bits of data interpreted as a raw bytes
   unsigned int frequency;   // stores the frequency in which the 8bits appear
   struct heap_node* left;   // link to left child
   struct heap_node* right;  // link to right child
 } hnode;
 
 typedef struct heap_tree {    // implementation of a min-heap
-  unsigned int dim;    // current dimension (size) of the min-heap
-  unsigned int cap;    // maximum dimension (size) of the min-heap
+  unsigned int size;    // current size of the min-heap
+  unsigned int cap;    // maximum size of the min-heap
   struct heap_node** arr;   // array of min-heap nodes, each node can hold an heap
 } heap;
 
 
 
-hnode* create_node(unsigned char, unsigned int);
-heap* create_heap(unsigned int);
-void swap_nodes(hnode**, hnode**);
+hnode* create_node(byte, int);
+heap* create_heap(int);
+
 void min_heapify(heap*, int);
-int has_one(heap*);
 hnode* get_min(heap*);
 void insert_node(heap*, hnode*);
 void build_min_heap(heap*);
-void print_arr(int*, int);
 int is_leaf(hnode*);
-heap* create_build_min_heap(unsigned char*, int*, int);
-hnode* build_huff_tree(unsigned char*, int*, int);
+heap* create_build_min_heap(byte*, int);
+hnode* build_huff_tree(byte*, int);
+
+static byte* process_data(byte*, int);
+static byte get_size(byte*);
+void compress(byte*, int);
+
+void print_arr(int*, int);
 void print_codes(hnode*, int*, int);
-void alpha_encode(unsigned char*, int*, int);
+void alpha_encode(byte*, int*, int);
 
 #endif // huffman_h
